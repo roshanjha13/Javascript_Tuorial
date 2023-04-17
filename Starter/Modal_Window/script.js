45bec600cabc3fef4b88678546b2ad2c386185d2
Roshan <roshan.jha@hih7.in>
2023-04-17 15:20:14 +0530

    mouseEvent

--- Starter/Modal_Window/script.js
+++ Starter/Modal_Window/script.js
@@ -1,12 +1,22 @@
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].addEventListener('click',));
-}
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
 
 // how to manupulate html classes with js :
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
